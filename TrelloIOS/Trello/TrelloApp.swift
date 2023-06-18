9530e0671a2e9add01828fb2bc3de83234d49a2b
bachbnt <bachbnt@gmail.com>
2023-06-18 15:44:25 +0700

    first commit

---
+++ TrelloIOS/Trello/TrelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrelloApp.swift
+//  Trello
+//
+//  Created by Bach Bui on 18/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct TrelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
